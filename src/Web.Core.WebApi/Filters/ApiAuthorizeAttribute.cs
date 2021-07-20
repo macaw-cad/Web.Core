2a7d88c9d8e9d166fb04698d274f4baa52af46ed
Danny de Haas <ddehaas@outlook.com>
2021-07-20 10:08:33 +0200

    Attribute added to check if user is authorized to access api-call

---
+++ src/Web.Core.WebApi/Filters/ApiAuthorizeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Web.Core.WebApi.Filters
+{
+    /// <summary>
+    /// Authenication Filter for use in API's.
+    /// Returns a 401 statuscode instead of redirecting to login page.
+    /// </summary>
+    public class ApiAuthorizeAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Method to check if user is authenicated.
+        /// </summary>
+        /// <param name="context"></param>
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.HttpContext?.User?.Identity?.IsAuthenticated ?? true)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
